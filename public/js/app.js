a90ccab8d5339752f3b80755277f55b44d8837ff
Ujjwal Bajagain <ubajagain@gmail.com>
2019-03-27 14:28:52 -0500

    detect active menu item

--- public/js/app.js
+++ public/js/app.js
@@ -50764,6 +50764,7 @@ var routes = [{
   component: __webpack_require__(/*! ./components/Profile */ "./resources/js/components/Profile.vue").default
 }];
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_0__["default"]({
+  mode: 'history',
   routes: routes
 });
 /**
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -74,7 +74,7 @@
                         </router-link>
                     </li>
                     <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link active">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-cogs"></i>
                             <p>
                                 Management
@@ -83,7 +83,7 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="#" class="nav-link active">
+                                <a href="#" class="nav-link">
                                     <i class="fa fa-circle-o nav-icon"></i>
                                     <p>Active Page</p>
                                 </a>
