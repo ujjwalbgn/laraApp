ab3838881f618bbd252fc19a393db73d10091572
Ujjwal Bajagain <ubajagain@gmail.com>
2019-03-27 18:17:38 -0500

    user form frontend created

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,15 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { Form, HasError, AlertError } from 'vform';
+
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
 
 import VueRouter from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard').default },
