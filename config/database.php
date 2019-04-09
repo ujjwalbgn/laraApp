e11b88c718ca0acba19e58135f12aed3dcf1d396
Ujjwal Bajagain <ubajagain@gmail.com>
2019-04-09 13:29:20 -0500

    config/database change for aws

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST') ?: $_SERVER['RDS_HOSTNAME'],
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE') ?: $_SERVER['RDS_DB_NAME'],
+            'username' => env('DB_USERNAME') ?: $_SERVER['RDS_USERNAME'],
+            'password' => env('DB_PASSWORD') ?: $_SERVER['RDS_PASSWORD'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
